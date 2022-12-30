06baa8585219664b6102deb8d97fb41d87895045
Maria T. Andersen <mteilmann93@gmail.com>
2022-12-30 22:21:07 +0100

    move chart values for the x and y axis to the top of the document for cleaner code

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
 console.log("Hello World");
 
-let myBarChart = new Chart("myBarChart", {
+let xValues = [
+  "Denmark",
+  "Germany",
+  "France",
+  "Spain",
+  "Italy",
+  "Greece",
+  "Turkey",
+  "Sweden",
+];
+let yValues = [55, 67, 23, 32, 68, 30, 25, 3];
+let barColors = [
+  "rgba(0,0,255,1.0)",
+  "rgba(0,0,255,0.8)",
+  "rgba(0,0,255,0.7)",
+  "rgba(0,0,255,0.6)",
+  "rgba(0,0,255,0.4)",
+  "rgba(0,0,255,0.2)",
+  "rgba(255,0,0,0.8)",
+  "rgba(255,0,0,0.6)",
+];
+
+new Chart("myBarChart", {
   type: "bar",
   data: {
-    labels: [
-      "Denmark",
-      "Germany",
-      "France",
-      "Spain",
-      "Italy",
-      "Greece",
-      "Turkey",
-      "Sweden",
-    ],
+    labels: xValues,
     datasets: [
       {
-        backgroundColor: [
-          "rgba(0,0,255,1.0)",
-          "rgba(0,0,255,0.8)",
-          "rgba(0,0,255,0.7)",
-          "rgba(0,0,255,0.6)",
-          "rgba(0,0,255,0.4)",
-          "rgba(0,0,255,0.2)",
-          "rgba(255,0,0,0.8)",
-          "rgba(255,0,0,0.6)",
-        ],
-        data: [55, 67, 23, 32, 68, 30, 25, 30],
+        backgroundColor: barColors,
+        data: yValues,
       },
     ],
   },
