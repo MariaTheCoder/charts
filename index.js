6fa2f7e0defd23576bd6339896be71bc17319ed9
Maria T. Andersen <mteilmann93@gmail.com>
2022-12-30 22:06:53 +0100

    Change colors of the columns.

--- index.js
+++ index.js
@@ -16,14 +16,14 @@ let myBarChart = new Chart("myBarChart", {
     datasets: [
       {
         backgroundColor: [
-          "green",
-          "red",
-          "yellow",
-          "blue",
-          "orange",
-          "brown",
-          "grey",
-          "darkblue",
+          "rgba(0,0,255,1.0)",
+          "rgba(0,0,255,0.8)",
+          "rgba(0,0,255,0.7)",
+          "rgba(0,0,255,0.6)",
+          "rgba(0,0,255,0.4)",
+          "rgba(0,0,255,0.2)",
+          "rgba(255,0,0,0.8)",
+          "rgba(255,0,0,0.6)",
         ],
         data: [55, 67, 23, 32, 68, 30, 25, 30],
       },
